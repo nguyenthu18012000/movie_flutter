31e944b4c9c1a705a5150523db1034e03ce4d00e
Thứ Nguyễn (BU2.HN) <thu.nguyen@bnksolution.com>
2023-02-03 17:27:04 +0700

    alksdjfh

--- lib/src/ui/login/login_form.dart
+++ lib/src/ui/login/login_form.dart
@@ -7,17 +7,46 @@ class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: InkWell(
-          child: Container(
-            child: Text('aaaaaaaaa'),
-          ),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const Login()),
-            );
-          },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              child: InkWell(
+                child: const Icon(Icons.arrow_back),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const Login()),
+                  );
+                },
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 30, bottom: 50),
+                    child: const Image(image: AssetImage('lib/assets/images/movie_logo.png'))
+                  ),
+                  const Text(
+                    'Login to Your Account',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                    ),
+                  )
+                ],
+              )
+            ),
+          ],
         ),
       ),
     );
