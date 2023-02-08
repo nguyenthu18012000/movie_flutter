8c6277f34f43a59a364f7c4b32101067a4bd6b6b
Thứ Nguyễn (BU2.HN) <thu.nguyen@bnksolution.com>
2023-02-08 17:24:28 +0700

    temp

--- lib/src/ui/home/home.dart
+++ lib/src/ui/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_flutter/src/blocs/popular_movies_bloc/bloc/popular_movies_bloc.dart';
 import 'package:movie_flutter/src/models/movie_popular_model.dart';
 import 'package:movie_flutter/src/resources/movies_popular_api.dart';
+import 'package:movie_flutter/src/resources/repositories.dart';
 import 'package:movie_flutter/src/ui/login/login_form.dart';
 
 class Home extends StatelessWidget {
@@ -27,21 +28,14 @@ class Home extends StatelessWidget {
                   const CircularProgressIndicator(),
                   ElevatedButton(
                     onPressed: () {},
-                    child: const Text("klasjdhfkajsdhfkjahsd")
+                    child: const Text("loading")
                   ),
                 ],
               )
             );
           }
           if (state is LoadedPopularMoviesState) {
-            return SafeArea(
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const LoginForm()));
-                },
-                child: Text("${state.moviePopular.page}")),
-            );
+            return HomeScreen(state: state);
           } else {
             return const Center(
               child: Text("something wrong"),
@@ -50,5 +44,67 @@ class Home extends StatelessWidget {
         },
       ),
     );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final LoadedPopularMoviesState state;
+  const HomeScreen({super.key, required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+          children: [
+            Text('ádfasdfasd'),
+            SizedBox(
+              height: 150,
+              child: GridView(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 1,
+                  mainAxisSpacing: 2,
+                ),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    child: Text('a;lskd'),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    child: Text('a;lskd'),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Text('a;lskd'),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Text('a;lskd'),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    child: Text('a;lskd'),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Text('a;lskd'),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )
+    );
+    // return SafeArea(
+    //   child: InkWell(
+    //       onTap: () {
+    //         Navigator.push(context,
+    //             MaterialPageRoute(builder: (_) => const LoginForm()));
+    //       },
+    //       child: Text("${state.moviePopular.totalPages}")),
+    // );
   }
 }
